011809bf5e1fed06e6ca0e3cd8d008f592db4dc1
Aloïs Micard <lunamicard@gmail.com>
2018-05-28 10:04:53 +0200

    Work on motorcycles list

--- source/src/main/webapp/management.js
+++ source/src/main/webapp/management.js
@@ -1,10 +1,18 @@
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<tr><td>{{ todo.brand }}</td></tr>'
+});
+
 $(function() {
     var management = new Vue({
         el: '#management',
         data: {
-            isLogged: window.localStorage.getItem('token') !== null
-        },
-        methods: {
+            isLogged: window.localStorage.getItem('token') !== null,
+            motorcycles: []
         }
     });
+
+    $.get("api/motorcycles", function (data) {
+        management.motorcycles = data;
+    })
 });
