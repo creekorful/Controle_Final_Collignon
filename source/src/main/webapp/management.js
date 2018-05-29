15fc6c631b04118fef4ba875680601b95de66d28
Aloïs Micard <amicard@eurodns.com>
2018-05-29 13:51:47 +0200

    Add primitive creation systme

--- source/src/main/webapp/management.js
+++ source/src/main/webapp/management.js
@@ -1,4 +1,34 @@
 $(function() {
+    var navbar = new Vue({
+        el: '#navbar',
+        data: {
+            isLogged: window.localStorage.getItem('token') !== null,
+            isAdmin: false
+        },
+        methods: {
+            logout: function() {
+                window.localStorage.clear();
+                window.location.reload();
+            },
+            showAdd: function() {
+                createMotorcycle.isShow = true;
+                editMotorcycle.motorcycle = undefined;
+                details.motorcycle = undefined;
+            }
+        }
+    });
+
+    $.ajax({
+           url: "api/session/isAdmin",
+           dataType: 'json',
+           success: function(data, status) {
+               navbar.isAdmin = data;
+           },
+           beforeSend: function(xhr, settings) {
+               xhr.setRequestHeader('Authorization','Bearer ' + window.localStorage.getItem('token'));
+           }
+       });
+
     var management = new Vue({
         el: '#management',
         data: {
@@ -28,12 +58,14 @@ $(function() {
                 $.get("api/motorcycles/" + id, function (data) {
                     details.motorcycle = data;
                     editMotorcycle.motorcycle = undefined;
+                    createMotorcycle.isShow = false;
                 });
             },
             editItem: function(id) {
                 $.get("api/motorcycles/" + id, function (data) {
                     details.motorcycle = undefined;
                     editMotorcycle.motorcycle = data;
+                    createMotorcycle.isShow = false;
                 });
             }
         }
@@ -78,6 +110,41 @@ $(function() {
                        $.get("api/motorcycles", function (data) {
                            management.motorcycles = data;
                            editMotorcycle.motorcycle = undefined;
+                       });
+                   },
+                   beforeSend: function(xhr, settings) {
+                       xhr.setRequestHeader('Authorization','Bearer ' + window.localStorage.getItem('token'));
+                   }
+               });
+            }
+        }
+    });
+
+    var createMotorcycle = new Vue({
+        el: '#createMotorcycle',
+        data: {
+            isShow: false,
+            motorcycle: {
+                brand: '',
+                model: '',
+                fiscalPower: 0,
+                tireType: '',
+                engineId: 0
+            }
+        },
+        methods: {
+            createMotorcycle: function() {
+                $.ajax({
+                   url: "api/motorcycles/",
+                   contentType: "application/json; charset=utf-8",
+                   dataType: 'json',
+                   type: 'POST',
+                   data: JSON.stringify(createMotorcycle.motorcycle),
+                   success: function(data) {
+                       $.get("api/motorcycles", function (data) {
+                           management.motorcycles = data;
+                           editMotorcycle.motorcycle = undefined;
+                           createMotorcycle.isShow = false;
                        });
                    },
                    beforeSend: function(xhr, settings) {
